1a050ed5ac0e33f363d6cb520211c36eaa45dcbd
vmsdodla <vmsdodla@gmail.com>
2023-06-20 14:38:57 +0530

    Update streamlit_app.py
    removing raw JSON

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ import requests
 streamlit.header("Fruityvice Fruit Advice!")
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+#streamlit.text(fruityvice_response.json())
 
 # write dat to screen? 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
