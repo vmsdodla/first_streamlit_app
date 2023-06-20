128eec88e22d7a0f799c05d70e3c7d581ad11d79
vmsdodla <vmsdodla@gmail.com>
2023-06-20 14:37:15 +0530

    Update streamlit_app.py
    presenting data as table

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,3 +21,8 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+# write dat to screen? 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# Output in screen as table
+streamlit.dataframe(fruityvice_normalized)
